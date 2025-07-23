3937e53045739195c9b32e0778e7c19ae3ba1445
Fanuel Hindeya <154921331+fanuelH@users.noreply.github.com>
2025-07-23 17:31:11 +0300

    feat: add leap year function

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  let isYearDivisibleByFour = year % 4 === 0;
+  let isCentury = year % 100 === 0;
+  let isYearDivisibleByFourHundred = year % 400 === 0;
+  if (isYearDivisibleByFour && (!isCentury || isYearDivisibleByFourHundred)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
