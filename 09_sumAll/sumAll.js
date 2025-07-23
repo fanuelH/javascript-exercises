062c9fd978a00905a977e631543fae655411d981
Fanuel Hindeya <154921331+fanuelH@users.noreply.github.com>
2025-07-23 16:41:45 +0300

    feat: add sum all function

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2)
+  )
+    return "ERROR";
+  let total = 0;
+  if (num1 > num2) {
+    for (let i = num2; num1 >= i; i++) {
+      total += i;
+    }
+  } else {
+    for (let i = num1; num2 >= i; i++) {
+      total += i;
+    }
+  }
+  return total;
 };
 
 // Do not edit below this line
