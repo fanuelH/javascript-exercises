114683f71528b6db62fa4ec08f241fec0bea92b1
Fanuel Hindeya <154921331+fanuelH@users.noreply.github.com>
2025-07-29 16:42:35 +0300

    feat: add findTheOldest exercise solution

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people
+    .map((item) => {
+      let calcYearOfDeath = item.yearOfDeath;
+      if (!calcYearOfDeath) {
+        calcYearOfDeath = new Date().getFullYear();
+      }
+      let age = calcYearOfDeath - item.yearOfBirth;
+      item.age = age;
+      return item;
+    })
+    .sort((a, b) => a.age - b.age)[people.length - 1];
 };
 
 // Do not edit below this line
