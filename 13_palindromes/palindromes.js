1e5b392012a4cac1300b719662ff219be4ed99cc
Fanuel Hindeya <154921331+fanuelH@users.noreply.github.com>
2025-07-29 12:41:20 +0300

    feat: add palindromes exercise solutions

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const alphaNumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const cleanedString = str
+    .toLowerCase()
+    .split("")
+    .filter((item) => alphaNumeric.includes(item))
+    .join("");
+  return cleanedString === cleanedString.split("").reverse().join("");
 };
 
 // Do not edit below this line
