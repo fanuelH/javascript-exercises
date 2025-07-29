33d466f25d495f4aed261eb7b490cc6560297190
Fanuel Hindeya <154921331+fanuelH@users.noreply.github.com>
2025-07-29 14:14:18 +0300

    feat: add fibonacci exercise solution

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  let count;
+  if (typeof n !== "number") {
+    count = parseInt(n);
+  } else {
+    count = n;
+  }
 
+  if (count < 0) return "OOPS";
+  if (count === 1) return count;
+
+  let preNum = 0;
+  let currNum = 1;
+  let fibonacci = 0;
+  for (let i = 2; i <= count; i++) {
+    fibonacci = preNum + currNum;
+    preNum = currNum;
+    currNum = fibonacci;
+  }
+  return fibonacci;
 };
 
 // Do not edit below this line
